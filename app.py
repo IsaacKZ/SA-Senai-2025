43256c6f3e8e7a50ab9db32d85c918d30dad8586
Isaac <isaackz2008@gmail.com>
2025-10-19 20:02:53 -0300

    erros de sintaxe + criação do html login

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('login')
+@app.route('/login', METHODS=['GET'])
 def login():
     return render_template("login.html")
 
